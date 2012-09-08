8772763d638e6c2bc37a041f10706829ff794710
math4tots <math4tots@gmail.com>
2012-09-08 00:30:02 -0700

    first commit

---
+++ MayCal.py
@@ -0,0 +1,297 @@
+'''
+<-------------- imports and helpful global vars ------------------------------->
+'''
+from itertools import takewhile, dropwhile
+alnum_ = '1234567890_abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+
+
+'''
+<-------------- Lexer and other parser helpers -------------------------------->
+'''
+def isIdentifier(strg):
+    for c in strg:
+        if c not in alnum_: return False
+    return True
+
+def lex(strg):
+    return (strg.replace('(', ' ( ').
+                 replace(')', ' ) ').
+                 replace(',', ' , ').
+                 replace('\\', ' \\ ').
+                 split())
+                
+'''
+<-------------- Parser methods ------------------------------------------------>
+'''
+def parse(tokens):
+    tokens = parseParenthesis(tokens)
+    tokens = parseLambdaExpr(tokens)
+    tokens = parseIfExpr(tokens)
+    tokens = parseBinop(tokens, {'^': 'exponent'}, isLeftAssociative = False)
+    tokens = parseBinop(tokens, {'*': 'multiply', '/': 'divide'})
+    tokens = parseBinop(tokens, {'+': 'add', '-': 'subtract'})
+    tokens = parseBinop(tokens, {'%': 'modulo'})
+    tokens = parseBinop(tokens, {'=': 'equals', '<': 'lessThan', '>': 'greaterThan'})
+    # if len(tokens) != 1, then parse isn't complete yet
+    return tokens[0]
+    
+    
+def parseParenthesis(tokens):
+    
+    while '(' in tokens:
+        # Find the first inner most parenthesis
+        b = 0
+        while tokens[b] != ')': b += 1
+    
+        a = b-1
+        while tokens[a] != '(': a -= 1
+        
+        if a > 0 and (isinstance(tokens[a-1],list) or isIdentifier(tokens[a-1])):
+            # If the left parenthesis is directly preceded by
+            # a list or string, this is a function call
+            fn = tokens[a-1]
+            args = []
+            
+            notComma = lambda x: x != ','
+            buffer = tokens[a:b] # ( ... , ... , (no end parenthesis)
+            while len(buffer) > 0:
+                buffer = buffer[1:] # remove either ( or comma
+                args.append(parse(list(takewhile(notComma, buffer))))
+                buffer = list(dropwhile(notComma, buffer))
+                
+            tokens = tokens[:a-1] + [ [fn] + args ] + tokens[b+1:]
+        else:
+            # Otherwise, the parenthesis was just for grouping
+            tokens = tokens[:a] + [ parse(tokens[a+1:b]) ] + tokens[b+1:]
+            
+    return tokens
+    
+def parseLambdaExpr(tokens):
+    while '\\' in tokens:
+        a = 0
+        while tokens[a] != '\\': a += 1
+        
+        b = a + 1
+        while tokens[b] != '->': b += 1
+        
+        argNames = tokens[a+1:b]
+        expr = parse(tokens[b+1:])
+        
+        tokens = tokens[:a] + [ ['lambda', argNames, expr] ]
+    return tokens
+        
+        
+def parseIfExpr(tokens):
+    while 'if' in tokens:
+        # Randomly pick an 'if', 'then', 'else', that will work.
+        # As a result it will be bad practice to nest if statements without parenthesis
+        a = 0
+        while tokens[a] != 'if': a += 1
+        
+        b = a + 1
+        while tokens[b] != 'else': b += 1
+        
+        tokens = [ ['if_function', parse(tokens[a+1:b]), parse(tokens[:a]), parse(tokens[b+1:]) ] ]
+        
+    return tokens
+    
+    
+def parseBinop(tokens, operators, isLeftAssociative = True):
+    def containsOperator(tokens):
+        for token in tokens:
+            if isinstance(token,str) and token in operators: return True
+        return False
+        
+    while containsOperator(tokens):
+        i = 1 if isLeftAssociative else len(tokens) - 2
+        
+        if isLeftAssociative:
+            while tokens[i] not in operators: i += 1
+        else:
+            while tokens[i] not in operators: i -= 1
+            
+        tokens = tokens[:i-1] + [ [operators[tokens[i]], tokens[i-1], tokens[i+1]] ] + tokens[i+2:]
+        
+    return tokens
+        
+'''
+<-------------- Built-in functions -------------------------------------------->
+'''
+
+def MayCalcFunc(fn):
+    def wrapper(ev, *args):
+        return fn( *list(map(ev, args)) )
+    return wrapper
+    
+@MayCalcFunc
+def Add(a,b): return a + b
+
+@MayCalcFunc
+def Subtract(a,b): return a - b
+
+@MayCalcFunc
+def Multiply(a,b): return a * b
+
+@MayCalcFunc
+def Divide(a,b): return a / b
+
+@MayCalcFunc
+def Exponent(a,b): return a ** b
+
+@MayCalcFunc
+def Equals(a,b): return a == b
+
+@MayCalcFunc
+def LessThan(a,b): return a < b
+
+@MayCalcFunc
+def GreaterThan(a,b): return a > b
+
+@MayCalcFunc
+def Modulo(a,b): return a % b
+
+def If_function(ev, condition, a, b):
+    if ev(condition): return ev(a)
+    return ev(b)
+
+def Lambda(ev, argNames, expr):
+    def applyBindings(expr, bindings):
+        if isinstance(expr,list):
+            for i in range(len(expr)):
+                if isinstance(expr[i],str) and expr[i] in bindings:
+                    expr[i] = bindings[expr[i]]
+                elif isinstance(expr[i], list):
+                    applyBindings(expr[i],bindings)
+    
+    def copyListFunc(lis):
+        copyList = lis[:]
+        for i in range(len(lis)):
+            if isinstance(copyList[i],list):
+                copyList[i] = copyListFunc(copyList[i])
+        return copyList
+
+    @MayCalcFunc
+    def wrapper(*args):
+        bindings = {k:v for k,v in zip(argNames,args)}
+        copyExpr = copyListFunc(expr) if isinstance(expr,list) else expr
+        applyBindings(copyExpr, bindings)
+        return ev(copyExpr)
+    return wrapper
+
+'''
+<-------------- Evaluator stuff ----------------------------------------------->
+'''
+
+class Evaluator(object):
+    def __init__(self, env):
+        self.env = env
+        
+    def __call__(self, expr):
+        ret = expr
+        if isinstance(expr, list):
+            ret = self(expr[0])(self, *expr[1:])
+        elif isinstance(expr, str):
+            try:        ret = int(expr)
+            except:
+                try:    ret = float(expr)
+                except: ret = self.env[expr]
+        return ret
+    
+
+class Environment(object):
+    def __init__(self):
+        self.base = {
+            'True': True,
+            'False': False,
+        
+            'add': Add,
+            'subtract': Subtract,
+            'multiply': Multiply,
+            'divide': Divide,
+            'exponent': Exponent,
+            'equals': Equals,
+            'lessThan': LessThan,
+            'greaterThan': GreaterThan,
+            'modulo': Modulo,
+            'lambda': Lambda,
+            'if_function': If_function,
+        }
+        
+        self.env = {}
+        
+    def __getitem__(self, key):
+        try:    return int(key)
+        except:
+            try:    return float(key)
+            except: pass
+        if key in self.env: return self.env[key]
+        return self.base[key]
+        
+    def __setitem__(self, key, value):
+        self.env[key] = value
+        
+    def clear(self):
+        self.env = {}
+    
+    def infoStr(self, includeBase = False):
+        ret = 'in user environment:\n'
+        for key in self.env:
+            ret += '%-10s : %s\n' % (key, self.env[key])
+        if includeBase:
+            ret += '\nin base environment:\n'
+            for key in self.base:
+                ret += '%-10s : %s\n' % (key, self.base[key])
+        return ret
+
+class Interpreter(object):
+    def __init__(self):
+        self.env = Environment()        
+        self.ev = Evaluator(self.env)
+        
+    def __call__(self, strg):
+        tokens = lex(strg)
+        ret = None
+        if tokens == ['who']:
+            print(self.env.infoStr())
+            
+        elif tokens == ['whos']:
+            print(self.env.infoStr(includeBase=True))
+            
+        elif tokens == ['clear']:
+            self.env.clear()
+            
+        elif ':=' in tokens:
+            # If '=' token is there, there's some sort of assignment
+            notEq = lambda x : x != ':='
+            
+            var = parse(list(takewhile(notEq, tokens)))
+            exprTokens = list(dropwhile(notEq,tokens))[1:]
+            expr = parse(exprTokens)
+            
+            if isinstance(var,list):
+                # If the lhs is a list, we're defining some sort of function.
+                # This allows function shorthand
+                self.env[var[0]] = self.ev( parse(['\\'] + var[1:] + ['->'] + exprTokens) )
+            else:
+                # Otherwise, we have normal assignment where we assign
+                # a single value
+                self.env[var] = self.ev(expr)
+            
+        else:
+            ret = self.ev(parse(tokens))
+            
+        return ret
+        
+def main():
+    i = Interpreter()
+    try:
+        inp = raw_input('>> ')
+        while True:
+            ret = i(inp)
+            if ret != None: print(ret)
+            inp = raw_input('>> ')
+    except EOFError:
+        pass
+    print('')
+        
+if __name__ == '__main__': main()
---
+++ MayCalEditor.py
@@ -0,0 +1,129 @@
+#!/usr/bin/python
+import Tkinter, tkFont, tkFileDialog, os
+from MayCal import Interpreter
+
+class MathEditor(Tkinter.Tk):
+    def __init__(self, fileName = None, cwd = os.getcwd()):
+        Tkinter.Tk.__init__(self)
+        
+        # Create some state variables
+        self.saveFileName   = fileName
+        self.cwd            = cwd
+        
+        # Build the menu bar
+        menubar = Tkinter.Menu(self)
+        self.config(menu=menubar)
+        
+        fileMenu = Tkinter.Menu(menubar, tearoff=False)
+        fileMenu.add_command(label="Exit", underline=1, command=self.wrappedQuit, accelerator="Ctrl+Q")
+        fileMenu.add_command(label="Open", underline=1, command=self.openFile)
+        fileMenu.add_command(label="Save", underline=1, command=self.save, accelerator="Ctrl+S")
+        fileMenu.add_command(label="Save As", underline=1, command=self.saveAs)
+        
+        editMenu = Tkinter.Menu(menubar, tearoff=False)
+        editMenu.add_command(label="Run MayCal", underline=1, command=self.evaluate, accelerator="Ctrl+Y")
+        editMenu.add_command(label="increase font size", underline=1,
+                             command=self.increaseFont,
+                             accelerator="Ctrl+=")
+        editMenu.add_command(label="decrease font size", underline=1,
+                             command=self.decreaseFont,
+                             accelerator="Ctrl+-")
+        
+        menubar.add_cascade(label="File", underline=0, menu=fileMenu)
+        menubar.add_cascade(label="Edit", underline=0, menu=editMenu)
+        
+        # bind hotkeys
+        self.bind_all("<Control-q>", self.wrappedQuit)
+        self.bind_all("<Control-s>", self.save)
+        self.bind_all("<Control-y>", self.evaluate)
+        self.bind_all("<Control-=>", self.increaseFont)
+        # self.bind_all("<Control-->", self.decreaseFont)
+        
+        # Build widgets
+        self.customFont = tkFont.Font(family="Courier", size=18)
+        self.mathText = Tkinter.Text(font = self.customFont)
+        self.mathText.pack(fill=Tkinter.BOTH, expand=1)
+        
+    def wrappedQuit(self, event=None):
+        self.quit()
+        
+    def save(self, event=None):
+        if self.saveFileName == None:   self.saveAs()
+        else:                           self.saveGivenFileName()
+        
+    def saveAs(self, event=None):
+        title = "Save file as..."
+        self.cwd = os.path.dirname(self.saveFileName) if self.saveFileName != None else self.cwd
+        
+        self.saveFileName = tkFileDialog.asksaveasfilename(parent=self, initialdir=self.cwd, title=title)
+        
+        if self.saveFileName == '': self.saveFileName = None
+        else:                       self.saveGivenFileName()
+        
+    def openFile(self, event=None):
+        title = "Select file to open"
+        self.cwd   = os.path.dirname(self.saveFileName) if self.saveFileName != None else self.cwd
+        
+        self.saveFileName = tkFileDialog.askopenfilename(parent=self, initialdir=self.cwd, title=title)
+        
+        if self.saveFileName == '': self.saveFileName = None
+        else:                       self.openGivenFileName()
+        
+    # The following two methods assume self.saveFileName is a valid filename
+    def saveGivenFileName(self):
+        self.saveFileName = os.path.realpath(self.saveFileName)
+        self.title(self.saveFileName)
+        
+        self.cwd = self.saveFileName
+        f = open(self.saveFileName, 'w')
+        f.write(self.mathText.get("1.0", Tkinter.END))
+        f.close()
+        
+    def openGivenFileName(self):
+        self.saveFileName = os.path.realpath(self.saveFileName)
+        self.title(self.saveFileName)
+    
+        self.cwd = self.saveFileName
+        
+        f = open(self.saveFileName, 'r')
+        self.mathText.delete("1.0", Tkinter.END)
+        self.mathText.insert(Tkinter.END, f.read())
+        f.close()
+        
+    def increaseFont(self, event=None, amount=2):
+        self.customFont.configure(size= self.customFont['size'] + amount)
+        
+    def decreaseFont(self, event=None, amount=2):
+        self.customFont.configure(size= self.customFont['size'] - amount)
+        
+    def evaluate(self, event=None):
+        interp = Interpreter()
+        text = self.mathText.get("1.0", Tkinter.END)
+        
+        # text turns out to be a unicode object and so behaves weirdly ...
+        text = text.encode('ascii', 'ignore')
+        
+        text = text.splitlines()
+        newText = []
+        for line in text:
+            newText.append(line)
+            if line.startswith(';'):
+                result = interp(line[1:])
+                if result != None:
+                    newText.append(str(result))
+        self.mathText.delete("1.0",Tkinter.END)
+        self.mathText.insert(Tkinter.END, '\n'.join(newText))
+        
+        
+        
+if __name__ == '__main__':
+    import sys
+    app = None
+    if len(sys.argv) == 2:
+        app = MathEditor(fileName = sys.argv[1])
+        app.openGivenFileName()
+    else:
+        app = MathEditor()
+        app.title("MayCalEditor")
+    
+    app.mainloop()
